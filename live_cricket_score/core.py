8269775c38e3c24cd607f07fd7cc660961bce970
Kaustav Das <kaustav543@gmail.com>
2016-07-03 18:57:00 +0530

    Test CI

--- live_cricket_score/core.py
+++ live_cricket_score/core.py
@@ -17,66 +17,74 @@ id_arr = []
 si = ""
 de = ""
 
+
 def fetch_id():
-	request = Request('http://cricscore-api.appspot.com/csa')
-	response = urlopen(request)
-	id_arr = json.loads(response.read())
-	return id_arr
+    request = Request('http://cricscore-api.appspot.com/csa')
+    response = urlopen(request)
+    id_arr = json.loads(response.read())
+    return id_arr
+
 
 def build_menu():
-	menu = gtk.Menu()
+    menu = gtk.Menu()
 
-	id_arr = fetch_id()
+    id_arr = fetch_id()
 
-	for match in id_arr:
-		item_match = gtk.MenuItem(match["t1"] + ' vs ' + match["t2"])
-		item_match.connect('activate', select_active, match["id"])
-		menu.append(item_match)
+    for match in id_arr:
+        item_match = gtk.MenuItem(match["t1"] + ' vs ' + match["t2"])
+        item_match.connect('activate', select_active, match["id"])
+        menu.append(item_match)
 
-	item_quit = gtk.MenuItem('Quit')
-	item_quit.connect('activate', quit)
-	menu.append(item_quit)
+    item_quit = gtk.MenuItem('Quit')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
 
-	menu.show_all()
-	return menu
+    menu.show_all()
+    return menu
+
 
 def select_active(widget, id):
-	match_id.get()
-	match_id.put(id)
- 
-def quit(_):
-	notify.uninit()
-	gtk.main_quit()
+    match_id.get()
+    match_id.put(id)
 
-def show_notification():	
-	notify.init(APPINDICATOR_ID)
-	while True:
-		id = match_id.get()
-		match_id.put(id)
-		if id != 0:
-			request = Request('http://cricscore-api.appspot.com/csa?id=' + str(id))
-			response = urlopen(request)
-			if response.getcode() == 200:
-				match_data = json.loads(response.read())
-				si = match_data[0]["si"]
-				de = match_data[0]["de"]
-			notify.Notification.new(si, de, None).show()
-		time.sleep(60)
+
+def quit(_):
+    notify.uninit()
+    gtk.main_quit()
+
+
+def show_notification():
+    notify.init(APPINDICATOR_ID)
+    while True:
+        id = match_id.get()
+        match_id.put(id)
+        if id != 0:
+            request = Request('http://cricscore-api.appspot.com/csa?id=' + str(id))
+            response = urlopen(request)
+            if response.getcode() == 200:
+                match_data = json.loads(response.read())
+                si = match_data[0]["si"]
+                de = match_data[0]["de"]
+            notify.Notification.new(si, de, None).show()
+        time.sleep(60)
+
 
 def indicator_panel():
-	indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('icon.svg'), appindicator.IndicatorCategory.APPLICATION_STATUS)
-	indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-	indicator.set_menu(build_menu())
-	gtk.main()
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('icon.svg'),
+                                           appindicator.IndicatorCategory.APPLICATION_STATUS)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(build_menu())
+    gtk.main()
+
 
 def main():
-	signal.signal(signal.SIGINT, signal.SIG_DFL)
-	match_id.put(0)
-	i_panel = threading.Thread(target=indicator_panel)
-	notif = threading.Thread(target=show_notification)
-	i_panel.start()
-	notif.start()
-	
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    match_id.put(0)
+    i_panel = threading.Thread(target=indicator_panel)
+    notif = threading.Thread(target=show_notification)
+    i_panel.start()
+    notif.start()
+
 
 if __name__ == "__main__":
-	main()
+    main()
